2107bc22874fd33022212b4593819ac7f3ec7239
Jeffrey Huang <jeffreyhuang0331@gmail.com>
2018-09-27 10:46:50 +0800

    Changed home greeting copy

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Welcome to Laravel
                 </div>
 
                 <div class="links">
